4807b3aadf915f4be4107383de828f49cef502ba
Aldf76 <contasfelipe623@gmail.com>
2024-12-18 22:04:40 -0300

    criando projeto Typescript em React - análise de componente

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,15 @@
+
+import { useState } from 'react'
+import './App.css'
+import DataGridComponent from './components/DataGridComponent';
+ 
+function App() {
+ 
+  return (
+    <>
+      <DataGridComponent />
+    </>
+  )
+}
+ 
+export default App
